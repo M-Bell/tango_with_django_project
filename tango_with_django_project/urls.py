6440344eeee955e2ef2b2a3970dfa9c0c86f0941
Maksym Shyiko <2835038s@student.gla.ac.uk>
2023-02-08 11:48:38 +0000

    Chapter 8

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
+    #path('', views.index, name='index'),
+    path('rango/', include('rango.urls'), name='rango'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
